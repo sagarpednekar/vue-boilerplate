25129c7640725da018105f16a9201570c93a935a
Sagar Pednekar <sagarpednekar04@gmail.com>
2021-02-08 01:10:19 +0530

    Create component for add and list todos

---
+++ src/Components/CreateTodo/CreateTodo.vue
@@ -0,0 +1,20 @@
+<template>
+  <input type="text" v-model="todo" />
+  <button @click="addToDo">Add todo</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todo: "",
+    };
+  },
+  methods: {
+    addToDo: function () {
+      this.$emit("create-to-do", this.todo);
+      this.todo = "";
+    },
+  },
+};
+</script>
---
+++ src/Components/TodoListing/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Today on the List</div>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.title }}
+    </li>
+  </ul>
+</template>
+
+
+<script>
+export default {
+  props: ["todos"],
+};
+</script>
